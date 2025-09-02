96c51a380a1ceb39cf2d1cabc4c1e61bdd27c6f3
Pharox24 <abrahamzayed03@gmail.com>
2025-09-02 14:38:32 +0800

    redesigned the UI For the egcss site, basically every part of the page but the hero

---
+++ egcss_pages/logic.js
@@ -0,0 +1,7 @@
+document.querySelectorAll('.scroll-link').forEach(anchor => {
+    anchor.addEventListener('click', function(e) {
+        e.preventDefault();
+        const target = document.querySelector(this.getAttribute('href'));
+        target.scrollIntoView({ behavior: 'smooth' });
+    });
+});
---
+++ egcss_pages/tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+    theme: {
+      extend: {
+        screens: {
+          'xs': {'max': '639px'},  // Targets screens smaller than 640px
+        },
+      },
+    },
+    plugins: [],
+  };
+  
